9fc66eef673f017c52bac2dbe9a3c2d8b7e7e959
zongzhili <zongzhil@nycstudents.net>
2025-04-07 22:41:22 -0400

    parent and daughter

---
+++ vite-project/src/components/BookItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <li v-if="book">
+      <p><strong>Title:</strong> {{ book.title }}</p>
+      <p>
+        <strong>Author:</strong> {{ book.author_name ? book.author_name[0] : 'Unknown Author' }}
+      </p>
+      <p>
+        <strong>Year of Release:</strong> {{ book.first_publish_year || 'No overview available' }}
+      </p>
+
+      <img
+        v-if="book.cover_i"
+        :src="'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg'"
+        alt="Book Cover"
+        width="150"
+      />
+      <p v-else>No cover available</p>
+    </li>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  book: {
+    type: Object,
+  },
+})
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  border: 1px solid #ddd;
+  margin: 10px 0;
+  padding: 10px;
+}
+
+p {
+  margin: 5px 0;
+  color: rgb(168, 0, 22);
+  font-size: 18px;
+}
+
+h1 {
+  color: rgb(133, 0, 11);
+  font-size: 36px;
+}
+
+h3 {
+  color: rgb(133, 0, 11);
+}
+
+img {
+  width: 100px;
+  height: auto;
+}
+</style>
--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,72 +1,33 @@
 <template>
   <div>
     <h1>Lord of the Rings</h1>
-    <ul>
-      <li v-for="(book, index) in books" :key="index">
-        <p><strong>Title:</strong> {{ book.title }} </p>
-        <p><strong>Author:</strong> {{ book.author_name ? book.author_name[0] : 'Unknown Author' }}</p>
-        <p><strong>Year of Release:</strong> {{ book.first_publish_year ? book.first_publish_year[0] : 'No overview available' }}</p>
-        
-        <img :src="'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg'" alt="Book Cover" width="150"/>
-      </li>
-    </ul>
+    <BookItem v-for="(book, index) in books" :key="index" :book="book" />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted } from 'vue'
+import BookItem from '../components/BookItem.vue'
 
-const books = ref([]);
-
+const books = ref([])
 
 async function getData() {
   try {
-    let res = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
-    
-    if (!res.ok) throw new Error("Failed to fetch data");
-    let data = await res.json();
-    
-    books.value = data.docs;
+    let res = await fetch('https://openlibrary.org/search.json?q=the+lord+of+the+rings')
+
+    if (!res.ok) throw new Error('Failed to fetch data')
+    let data = await res.json()
+
+    books.value = data.docs
   } catch (error) {
-    console.error(error);
-    alert("Failed to fetch data");
+    console.error(error)
+    alert('Failed to fetch data')
   }
 }
 
 onMounted(() => {
-  getData();
-});
+  getData()
+})
 </script>
 
-<style scoped>
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  border: 1px solid #ddd;
-  margin: 10px 0;
-  padding: 10px;
-}
-
-p {
-  margin: 5px 0;
-  color: rgb(168, 0, 22);
-  font-size: 18px;
-}
-
-h1 {
-  color: rgb(133, 0, 11);
-  font-size: 36px;
-}
-
-h3 {
-  color: rgb(133, 0, 11);
-}
-
-img {
-  width: 100px;
-  height: auto;
-}
-</style>
+<style scoped></style>
