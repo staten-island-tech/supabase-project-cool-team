0eb2cd25bfd16367ed34f151e92417135603be64
zongzhili <zongzhil@nycstudents.net>
2025-04-21 13:44:20 -0400

    Update BookItem.vue

--- vite-project/src/components/BookItem.vue
+++ vite-project/src/components/BookItem.vue
@@ -1,43 +1,48 @@
 <template>
   <div>
     <ul class="list-none p-0">
-    <li v-if="book" class="border border-gray-300 my-2.5 p-2.5">
-      <p><strong>Title:</strong> {{ book.title }}</p>
-      <p>
-        <strong>Author:</strong> {{ book.author_name ? book.author_name[0] : 'Unknown Author' }}
-      </p>
-      <p>
-        <strong>Year of Release:</strong> {{ book.first_publish_year || 'No overview available' }}
-      </p>
+      <!-- First <li> for the book details -->
+      <li v-if="book" class="border border-gray-300 my-2.5 p-2.5">
+        <p><strong>Title:</strong> {{ book.title }}</p>
+        <p>
+          <strong>Author:</strong> {{ book.author_name ? book.author_name[0] : 'Unknown Author' }}
+        </p>
+        <p>
+          <strong>Year of Release:</strong> {{ book.first_publish_year || 'No overview available' }}
+        </p>
 
-      <img
-        v-if="book.cover_i"
-        :src="'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg'"
-        alt="Book Cover"
-        width="150"
-      />
-      <p v-else>No cover available</p>
-      <button @click="addToWatchlist(book)" class="mt-2 px-4 py-2 bg-red-700 text-white rounded hover:bg-red-800"
-      >Add to Watchlist</button>
-      <button type="submit">Search</button>
-  <div class="book-item-container">
-    <li v-if="book" class="book-item">
-      <div class="book-image">
         <img
           v-if="book.cover_i"
           :src="'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg'"
           alt="Book Cover"
-          class="book-cover"
+          width="150"
         />
         <p v-else>No cover available</p>
-      </div>
-      <div class="book-details">
-        <p><strong>Title:</strong> {{ book.title }}</p>
-        <p><strong>Author:</strong> {{ book.author_name ? book.author_name[0] : 'Unknown Author' }}</p>
-        <p><strong>Year of Release:</strong> {{ book.first_publish_year || 'No overview available' }}</p>
-      </div>
-    </li>
-  </ul>
+        
+        <button @click="addToWatchlist(book)" class="mt-2 px-4 py-2 bg-red-700 text-white rounded hover:bg-red-800">
+          Add to Watchlist
+        </button>
+        <button type="submit">Search</button>
+      </li>
+
+      <!-- Second <li> for the book cover and details -->
+      <li v-if="book" class="book-item">
+        <div class="book-image">
+          <img
+            v-if="book.cover_i"
+            :src="'https://covers.openlibrary.org/b/id/' + book.cover_i + '-M.jpg'"
+            alt="Book Cover"
+            class="book-cover"
+          />
+          <p v-else>No cover available</p>
+        </div>
+        <div class="book-details">
+          <p><strong>Title:</strong> {{ book.title }}</p>
+          <p><strong>Author:</strong> {{ book.author_name ? book.author_name[0] : 'Unknown Author' }}</p>
+          <p><strong>Year of Release:</strong> {{ book.first_publish_year || 'No overview available' }}</p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -45,9 +50,7 @@
 defineProps({
   book: Object
 })
-
 </script>
 
 <style scoped>
-
 </style>
