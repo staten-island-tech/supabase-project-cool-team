9f3f5f8feb8341d454632eb38dc61d0219caae17
zongzhili <zongzhil@nycstudents.net>
2025-05-05 13:50:45 -0400

    Update supabase.js

--- vite-project/src/views/supabase.js
+++ vite-project/src/views/supabase.js
@@ -1,4 +1,4 @@
-import  { createCilent } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = 'https://azktgkimeldnhwihpkze.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6a3Rna2ltZWxkbmh3aWhwa3plIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4NjE4NTEsImV4cCI6MjA2MTQzNzg1MX0.BFwjocv5o2cvkBlvlaGSvx_r4-F40E4PK0N4Jze6fJY'
@@ -30,13 +30,3 @@ async function signUp(email, password) {
     console.log('User logged in successfully:', user);
   }
 }
-
-async function logOut() {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error('Error during sign out:', error.message);
-    } else {
-      console.log('User logged out successfully');
-    }
-  }
-  
