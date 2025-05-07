a02bf94832dcccb2d80df966f96f767c6829f11a
zongzhili <zongzhil@nycstudents.net>
2025-05-07 13:59:36 -0400

    Create Auth.vue

---
+++ vite-project/src/views/Auth.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <input v-model="email" placeholder="Email" />
+    <input v-model="password" placeholder="Password" type="password" />
+    <button @click="signIn">Sign In</button>
+    <button @click="signUp">Sign Up</button>
+    <p v-if="error">{{ error }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { supabase } from '@/supabase'
+
+const email = ref('')
+const password = ref('')
+</script>
