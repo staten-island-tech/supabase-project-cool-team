ca1c981efcf468a49c14e998155df8513b1e5d88
zongzhili <zongzhil@nycstudents.net>
2025-05-30 14:10:05 -0400

    Update ReadListView.vue

--- vite-project/src/views/ReadListView.vue
+++ vite-project/src/views/ReadListView.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1 class="text-center text-2xl mb-4">Read List</h1>
-    <ul class="space-y-4">
-      <li
+    <button type="submit" class="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white font-semibold py-2 px-6 rounded-lg transition duration-200">Log Out</button>
+    <div class="space-y-4">
+      <div
         v-for="book in readlist"
         :key="book.key"
         class="bg-white shadow rounded p-4 flex flex-col items-center space-y-2"
@@ -21,10 +22,12 @@
           class="w-32 h-auto rounded-lg"
         />
         <p v-else class="text-gray-500">No cover available</p>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
+
+
 
 <script setup>
 import { useReadlistStore } from '../stores/readlistStore'
